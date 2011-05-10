82fcd5b8bdf881a425041080cd595f8dc15e9815
speadup <speadup@01d7fd69-4d27-cd72-2592-6f9a750b750e>
2011-05-10 09:33:31 +0000

    MSG

--- trunk/native/display.c
+++ trunk/native/display.c
@@ -33,7 +33,8 @@ WCHAR DisplayBuffer[1024];
 USHORT LinePos = 0;
 WCHAR PutChar[2] = L" ";
 UNICODE_STRING CharString = {2, 2, PutChar};
-
+BOOLEAN ECHO_STATUS = TRUE;
+BOOLEAN ERROR_STATUS = TRUE;
 /*++
  * @name RtlCliPrintString
  *
@@ -229,4 +230,168 @@ RtlCliDisplayString(IN PCH Message, ...)
     // Return to the caller
     //
     return Status;
+}
+
+/*********************************************************************************************************************/
+WCHAR ScreenBuffer[1024] ;
+VOID DisplayString(LPCWSTR lpwString)
+{
+   UNICODE_STRING us;
+   if(ECHO_STATUS){
+       RtlInitUnicodeString (&us, lpwString);
+       NtDisplayString (&us);
+   }
+}
+
+VOID Printf (char* fmt, ...)
+{
+   char buffer[512];
+   va_list ap;
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ECHO_STATUS){
+       va_start(ap, fmt);
+       vsprintf(buffer, fmt, ap);
+       va_end(ap);
+
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+VOID Print (char* buffer)
+{
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ECHO_STATUS){
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+
+VOID PutChar(WCHAR pChar)
+{
+    WCHAR PutChar[2] = L" ";
+    UNICODE_STRING CharString = {2, 2, PutChar};
+    if(ECHO_STATUS){
+        CharString.Buffer[0] = pChar;
+        return NtDisplayString(&CharString);
+    }
+}
+
+VOID ErrorPrintf (char* fmt, ...)
+{
+   char buffer[512];
+   va_list ap;
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ERROR_STATUS){
+       va_start(ap, fmt);
+       vsprintf(buffer, fmt, ap);
+       va_end(ap);
+
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+VOID ErrorPrint (char* buffer)
+{
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ERROR_STATUS){
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+VOID BufferPrintf (char* fmt, ...)
+{
+   char buffer[512];
+   va_list ap;
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ECHO_STATUS){
+       va_start(ap, fmt);
+       vsprintf(buffer, fmt, ap);
+       va_end(ap);
+
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       ScreenBufferCat(UnicodeString.Buffer);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+VOID BufferPrint (char* buffer)
+{
+   UNICODE_STRING UnicodeString;
+   ANSI_STRING AnsiString;
+   if(ECHO_STATUS)
+   {
+       RtlInitAnsiString (&AnsiString, buffer);
+       RtlAnsiStringToUnicodeString (&UnicodeString,
+                     &AnsiString,
+                     TRUE);
+       ScreenBufferCat(UnicodeString.Buffer);
+       NtDisplayString(&UnicodeString);
+       RtlFreeUnicodeString (&UnicodeString);
+   }
+}
+
+
+VOID BufferPutChar(WCHAR pChar)
+{
+    WCHAR PutChar[2] = L" ";
+    UNICODE_STRING CharString = {2, 2, PutChar};
+    if(ECHO_STATUS){
+        if(pChar == '\b')
+        {
+            ScreenBuffer[wcslen(ScreenBuffer)] = '\0';
+            CharString.Buffer[0] = '\r';
+            NtDisplayString(&CharString);
+            RtlInitUnicodeString (&CharString, ScreenBuffer);
+            NtDisplayString(&CharString);
+        }
+        else
+        {
+            CharString.Buffer[0] = pChar;
+            ScreenBufferCat(UnicodeString.Buffer);
+            NtDisplayString(&CharString);
+        }
+    }
+}
+
+VOID ScreenBufferCat(WCHAR* pBuffer)
+{
+    int len1 = wcslen(ScreenBuffer);
+    int len2 = wcslen(pBuffer);
+    if(len1 + len2 > 80)
+    {
+        wcscpy(ScreenBuffer, (WCHAR*)&pBuffer[len2 - (len1 + len2) % 80]);
+    }
+    else
+    {
+        wcscat(ScreenBuffer, pBuffer);
+    }
 }
